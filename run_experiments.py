08793b0d84391a5147d7f1286e08e0b7d67004cf
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2022-04-08 18:30:11 +0100

    reset settings

--- run_experiments.py
+++ run_experiments.py
@@ -12,33 +12,22 @@ SPMe = assemble_model({"name": "SPMe+SR", **options})
 DFN = assemble_model({"name": "DFN+SR", **options})
 
 models = [SPMe, DFN]
-# models = [DFN]
 
 # Define parameters
 param = set_parameters()
-# param.update(
-#     {
-#         "Exchange-current density for plating [A.m-2]": 1e-3,
-#         "Exchange-current density for stripping [A.m-2]": 0,
-#     }
-# )
 
 # Define experiment
-N_cycles = 10
-# save_at_cycles = [1]
-save_at_cycles = None
+N_cycles = 2000
+save_at_cycles = [1]
 C_ch = 1 / 3
-C_dch = 1
+C_dch = 2
 
 experiment = pybamm.Experiment(
     [
         ("Discharge at {}C until 2.5 V".format(C_dch),
-        "Rest for 30 minutes",
         "Charge at {}C until 4.2 V".format(C_ch),
-        "Hold at 4.2 V until C/20",
-        "Rest for 30 minutes",)
+        "Hold at 4.2 V until C/20")
     ] * N_cycles,
-    # cccv_handling="ode"
 )
 
 # Solve models
@@ -66,27 +55,27 @@ for model in models:
         "data",
         "sim_" + filename + "_{}.pkl".format(N_cycles),
     ))
-    sims.append(sim)
+#     sims.append(sim)
 
-pybamm.dynamic_plot(
-    sims,
-    output_variables=[
-        # [
-        #     "Inner negative electrode SEI concentration [mol.m-3]",
-        #     "Outer negative electrode SEI concentration [mol.m-3]",
-        # ],
-        # [
-        #     "X-averaged inner negative electrode SEI concentration [mol.m-3]",
-        #     "X-averaged outer negative electrode SEI concentration [mol.m-3]",
-        # ],
-        "Total SEI thickness [m]",
-        "X-averaged total SEI thickness [m]",
-        "Loss of capacity to SEI [A.h]",
-        # "Negative electrode porosity",
-        "X-averaged negative electrode porosity",
-        # "Negative electrode SEI interfacial current density",
-        "X-averaged SEI interfacial current density [A.m-2]",
-        # "Negative electrode interfacial current density",
-        "Terminal voltage [V]"
-    ]
-)
+# pybamm.dynamic_plot(
+#     sims,
+#     output_variables=[
+#         # [
+#         #     "Inner negative electrode SEI concentration [mol.m-3]",
+#         #     "Outer negative electrode SEI concentration [mol.m-3]",
+#         # ],
+#         # [
+#         #     "X-averaged inner negative electrode SEI concentration [mol.m-3]",
+#         #     "X-averaged outer negative electrode SEI concentration [mol.m-3]",
+#         # ],
+#         "Total SEI thickness [m]",
+#         "X-averaged total SEI thickness [m]",
+#         "Loss of capacity to SEI [A.h]",
+#         # "Negative electrode porosity",
+#         "X-averaged negative electrode porosity",
+#         # "Negative electrode SEI interfacial current density",
+#         "X-averaged SEI interfacial current density [A.m-2]",
+#         # "Negative electrode interfacial current density",
+#         "Terminal voltage [V]"
+#     ]
+# )
